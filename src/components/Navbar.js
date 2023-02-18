4b30517d462f8ec07ec391b280f6417571e513de
Sai <saiaungminkhant20@gmail.com>
2023-02-18 16:15:05 +0630

    improved navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,25 +1,26 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import logo from './images/logo.png';
 
 function CollapsibleExample() {
   return (
-    <Navbar collapseOnSelect expand="md" bg="transparent" variant="dark">
+    <Navbar className='nav' collapseOnSelect expand="md" bg="transparent" variant="dark">
       <Container>
  
-          <Nav className="me-auto">
-            <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Nav>
+            <Navbar.Brand href="#home"><img src={logo} alt='' className='logo' /></Navbar.Brand>
           </Nav>
 
-      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
           <Nav>
-            <Nav.Link href="#deets">More deets</Nav.Link>
-            <Nav.Link eventKey={2} href="#memes">
-              Dank memes
-            </Nav.Link>
+
+            <Nav.Link href="#deets"><h4>About</h4></Nav.Link>
+
+            <Nav.Link eventKey={2} href="#memes"><h4>Contact</h4></Nav.Link>
+
+            <Nav.Link eventKey={2} href="#memes"><h4>Contact</h4></Nav.Link>
+
           </Nav>
-        </Navbar.Collapse>
 
       </Container>
     </Navbar>
